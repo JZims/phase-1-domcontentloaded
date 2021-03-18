4ddd56ca1b7b87b2c731691505ef1f06365c2eb3
JZims <JZimmsGames@gmail.com>
2021-03-18 18:26:53 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+
+
+let swapText = document.querySelector("p")
+
+document.addEventListener("DOMContentLoaded", function() {
+    swapText.textContent= "This is really cool!"
+})
+
+console.log("This consolelog fires when js loads - before DOMContentLoaded is triggered")
